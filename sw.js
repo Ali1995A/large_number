1308871e179ba285756fd6694d87ee7d0261b426
Ali1995A <liexpress@gmail.com>
2026-01-15 15:54:03 +0800

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "candy-princess-big-number-v1";
+const CACHE = "candy-princess-big-number-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
